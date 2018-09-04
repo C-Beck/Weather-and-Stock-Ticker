439cf37ed27d6157330ac5a7f84790898d0e4356
C-Beck <C-Beck@users.noreply.github.com>
2018-09-04 04:53:38 +0000

    Starting on data parsing

--- Weather_Retrieval/getWeather.c
+++ Weather_Retrieval/getWeather.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <curl/curl.h>
 
 int main()
 {
     FILE *fp;
     fp=fopen("./icao_idents","r");
-    int ret = fclose(fp);
+    if(fp != NULL)
+    {
+	    char in[100];
+	    while(fgets(in, 100, fp)!=NULL)
+	    {
+		    in[strcspn(in, "\n")] = 0;
+		    printf("%s\n",in);
+	    }
+	    int ret = fclose(fp);
+    }
+    else{
+	    fprintf(stderr,"File either does not exist or cannot be read\n");
+    }
+
 }
