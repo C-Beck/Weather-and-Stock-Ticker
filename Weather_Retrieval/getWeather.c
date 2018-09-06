e977ca093e3d8a243cd00d2fbb5f4fe373c8221c
C-Beck <C-Beck@users.noreply.github.com>
2018-09-06 03:32:42 +0000

    retrieve idents from files and stores in variable

--- Weather_Retrieval/getWeather.c
+++ Weather_Retrieval/getWeather.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <curl/curl.h>
 
-int main()
+int getIdents(char *idents[])
 {
+    int numIdents = 0;
     FILE *fp;
     fp=fopen("./icao_idents","r");
     if(fp != NULL)
     {
-	    char in[100];
-	    while(fgets(in, 100, fp)!=NULL)
+	    char in[6];
+	    while(fgets(in,6,fp)!=NULL)
+	    {
+		    numIdents++;
+	    }
+	    //printf("%d\n",numIdents);
+	    //printf("%x\n",idents);
+	    rewind(fp);
+	    int i = 0;
+	    while(fgets(in, 6, fp)!=NULL)
 	    {
 		    in[strcspn(in, "\n")] = 0;
-		    printf("%s\n",in);
+		    //printf("%s\n",in);
+		    idents[i]=malloc(5);
+		    strncpy(idents[i],in,5);
+		    i++;
+	    }
+	    for(int j=0; j<numIdents; j++)
+	    {
+		    printf("%s\n",idents[j]);
 	    }
 	    int ret = fclose(fp);
     }
     else{
 	    fprintf(stderr,"File either does not exist or cannot be read\n");
     }
+    return(numIdents);
+}
 
+int main()
+{
+    char *idents[64];
+    int numIdents = getIdents(idents);
+    
 }
